d93394d387c6e00319f4120e9a06540ea9bc394e
Aaron Gorka <aaron.gorka@amaysim.com.au>
2018-03-28 14:51:25 +1100

    Add error handling if no instances in cluster

--- serverless_ecs_metrics/serverless_ecs_metrics.py
+++ serverless_ecs_metrics/serverless_ecs_metrics.py
@@ -20,38 +20,52 @@ def handler(event, context):
     cloudwatch = boto3.client('cloudwatch')
 
     clusters = [x.split('/')[-1] for x in ecs.list_clusters()['clusterArns']]
-    logging.debug(json.dumps({'message': 'getting clusters', 'clusters': clusters}))
+    logging.info(json.dumps({'message': 'getting clusters', 'clusters': clusters}))
 
     MetricData = []
     for cluster in clusters:
         metrics = []
-        mem = cloudwatch.get_metric_statistics(
-            Namespace='AWS/ECS',
-            MetricName='MemoryReservation',
-            StartTime=datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
-            EndTime=datetime.datetime.utcnow(),
-            Period=300, 
-            Statistics=['Average'],
-            Dimensions=[{'Name': 'ClusterName', 'Value': cluster}]
-        )['Datapoints'][0]['Average']
-        cpu = cloudwatch.get_metric_statistics(
-            Namespace='AWS/ECS',
-            MetricName='CPUReservation',
-            StartTime=datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
-            EndTime=datetime.datetime.utcnow(),
-            Period=300, 
-            Statistics=['Average'],
-            Dimensions=[{'Name': 'ClusterName', 'Value': cluster}]
-        )['Datapoints'][0]['Average']
+        cpu = None
+        mem = None
+        try:
+            response = cloudwatch.get_metric_statistics(
+                Namespace='AWS/ECS',
+                MetricName='MemoryReservation',
+                StartTime=datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
+                EndTime=datetime.datetime.utcnow(),
+                Period=300,
+                Statistics=['Average'],
+                Dimensions=[{'Name': 'ClusterName', 'Value': cluster}]
+            )
+            mem = response['Datapoints'][0]['Average']
+            logging.info(json.dumps({"message": "getting memory", "mem": mem}))
+        except:
+            logging.exception(json.dumps({"message": "getting memory", "response": response}))
 
-        combined = max([cpu, mem])
+        try:
+            response = cloudwatch.get_metric_statistics(
+                Namespace='AWS/ECS',
+                MetricName='CPUReservation',
+                StartTime=datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
+                EndTime=datetime.datetime.utcnow(),
+                Period=300,
+                Statistics=['Average'],
+                Dimensions=[{'Name': 'ClusterName', 'Value': cluster}]
+            )
+            cpu = response['Datapoints'][0]['Average']
+            logging.info(json.dumps({"message": "getting cpu", "cpu": cpu}))
+        except:
+            logging.exception(json.dumps({"message": "getting cpu", "response": response}))
 
-        MetricData.append({
-            'MetricName': 'CombinedReservation',
-            'Dimensions': [{'Name': 'ClusterName', 'Value': cluster}],
-            'Value': combined,
-            'Unit': 'Percent'
-        })
+        if cpu and mem:
+            combined = max([cpu, mem])
+
+            MetricData.append({
+                'MetricName': 'CombinedReservation',
+                'Dimensions': [{'Name': 'ClusterName', 'Value': cluster}],
+                'Value': combined,
+                'Unit': 'Percent'
+            })
 
     logging.debug(json.dumps(MetricData))
 
